95cca0082c1a0f6cc3fcfb3e2b1153584a37e51f
brangapp <brangapp@cisco.com>
2020-09-11 16:43:18 +0530

    Author: brangapp Fabric Performance Monitor

--- fpm.py
+++ fpm.py
@@ -22,6 +22,7 @@ class FPM:
         Sample output of this proc is as follows
         {'TABLE_fpm_registration_summary': {'ROW_fpm_registration_summary': [{'vsan': 1001, 'TABLE_device_info': {'ROW_device_info': [{'fcid': '0xec0000', 'pwwn': '21:00:f4:e9:d4:54:ad:a1', 'fpin_descriptor': ['Congestion Notification', 'Peer Congestion Notification', 'Link Integrity Notification', 'Delivery Notification'], 'congestion_signal': ['Warning', 'Alarm']}]}}]}}
         """
+        #out = self._sw.show("show analytics system-load", use_ssh=True)
         out = self._sw.show("show fpm registration summary")
         if out:
             regDict = {}
